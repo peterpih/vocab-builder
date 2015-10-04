fa30e7a124f95cd829d469dc7ce6f20709a389d1
peterpih <ppih@panix.com>
2015-10-04 18:00:19 +0100

    Cycle through list of words sequentially by :id, when at end of list, circle back to beginning

--- app/controllers/vocab_words_controller.rb
+++ app/controllers/vocab_words_controller.rb
@@ -4,18 +4,66 @@ class VocabWordsController < ApplicationController
   # GET /vocab_words
   # GET /vocab_words.json
   def index
+    logger.debug "-----index-----"
     @vocab_words = VocabWord.all
   end
 
   # GET /vocab_words/1
   # GET /vocab_words/1.json
   def show
-    @vocab_word = VocabWord.take
+    logger.debug "-----show----- " + @vocab_word.word
+    #@vocab_word = VocabWord.first
+    logger.debug "-----id----- " + @vocab_word.id.to_s
   end
 
   # GET /vocab_words/new
   def new
     @vocab_word = VocabWord.new
+  end
+
+  def next
+    logger.debug "-----next-----"
+    url = request.original_url
+    logger.debug "=====" + url.to_s + "====="
+    u = URI.parse(url)
+    logger.debug "-----scheme " + u.scheme + "-----"
+    logger.debug "-----host " + u.host + "-----"
+    logger.debug "-----path " + u.path + "-----"
+    logger.debug "-----query " + u.query.to_s + "-----"
+    domain = u.host
+    if (domain == "localhost")
+      logger.debug "-----localhost-----"
+      domain = domain + ":3000"
+    end
+    logger.debug "-----domain " + domain + "-----"
+    if u
+      p = CGI.parse(u.query)
+      logger.debug "-----u " + u.query.to_s + "-----"
+      logger.debug "-----p " + p.to_s + "-----"
+
+      # p is now {"id"=>["4"], "empid"=>["6"]
+      id = p['id'].first
+    else
+      id = 0
+    end
+    logger.debug "-----id: " + id.to_s + "-----"
+    #redirect_to @new_url
+    #
+    @vocab_word = VocabWord.where("id > ?", id).first
+    logger.debug "-----return: " + @vocab_word.to_s
+    if (! @vocab_word)
+      @vocab_word = VocabWord.where("id > ?", 0).first  # circle back for now
+    end
+    next_id = @vocab_word.id
+    logger.debug "-----next id: " + next_id.to_s + "-----"
+    @next_url = u.scheme + "://" + domain + u.path + "?id=" + next_id.to_s
+    logger.debug "-----next_url: " + @next_url + "-----"
+    #@vocab_word = VocabWord.find(id.next)
+    #logger.debug @vocab_word.id.to_s
+    #@vocab_word = VocabWord.where("id > ?", id).first
+    #logger.debug "-----back----- " + @vocab_word.id.to_s
+    #@vocab_word = VocabWord.first
+
   end
 
   # GET /vocab_words/1/edit
@@ -65,11 +113,12 @@ class VocabWordsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_vocab_word
+      logger.debug "-----set_vocab_word----- " + params[:id].to_s
       @vocab_word = VocabWord.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def vocab_word_params
-      params.require(:vocab_word).permit(:word, :lesson)
+      params.require(:vocab_word).permit(:id, :word, :lesson)
     end
 end
